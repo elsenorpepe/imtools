4726187b79bae8f4660a7a861582c3240fa09c64
Russ Pepe <russellpepe@gmail.com>
2019-11-22 21:57:31 -0600

    pathlib info in app.py

---
+++ app.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+# dealing with linux/windows filepaths
+# https://medium.com/@ageitgey/python-3-quick-tip-the-easy-way-to-deal-with-file-paths-on-windows-mac-and-linux-11a072b58d5f
